162a468916c60d06bece93754e032c4b0c1b70bd
pgonzalo <pintadogonzalo2@gmail.com>
2020-09-01 20:04:44 -0300

    +Task + Tasks

---
+++ src/components/Task.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Task extends Component{
+    render () {
+        const {task} = this.props;
+        return <div>
+        {task.description} - 
+        {task.done} - 
+        {task.id}
+        {task.title} - 
+        <input type="checkbox" />
+        <button>
+            x
+        </button>
+        </div>
+    }
+}
+
+export default Task;
